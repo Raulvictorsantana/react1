5a5965375459a3ab09aaaddc26f396680f0c6a0d
Raulvictorsantana <79764863+Raulvictorsantana@users.noreply.github.com>
2023-03-22 23:20:09 -0300

    inserindo dados no banco via api

--- src/Pages/NovoProjeto.js
+++ src/Pages/NovoProjeto.js
@@ -1,14 +1,42 @@
 import styles from './NovoProjeto.module.css'
+import {useNavigate}   from 'react-router-dom'
 import Formulario from '../projeto/Formulario'
 //botao nao esta recebendo o valor dinamico
+
+
 function NovoProjeto(){
-    return(
+   
+    const Navigate = useNavigate()
+
+    function createPost(project){
+        project.cost=0
+        project.services=[]
+        
+        fetch('http://localhost:5000/project',{
+         method:'POST',
+         headers: {
+            'Content-type':'application/json',
+            },
+            body: JSON.stringfy(project),
+
+        })
+
+        .then((resp)=>resp.json())
+        .then((data)=>{ 
+            console.log(data)
+            Navigate('projects',{messsage:'projeto criado com sucesso'})
+        })
+        .catch((err)=> console.log(err))
+        
+
+    }
+    return (
      <div className={styles.NovoProjeto_conatiner}>
                         
     <h1> Nova Pagina</h1>
     <p>Crie seu  projeto para adicionar os serviços </p>
-    <Formulario btnText="ola"/>
+    <Formulario handleSubmit={createPost} btnText="ola"/>
     </div>
     ) 
-}
+}   
 export default NovoProjeto
